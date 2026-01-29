faeb6d5b7f160b2ad219f5df02169954d9b62d79
Ifaz-Ikram <ifazmohomed@gmail.com>
2026-01-29 13:41:32 +0530

    .me

--- src/Pages/WelcomeScreen.jsx
+++ src/Pages/WelcomeScreen.jsx
@@ -114,14 +114,14 @@ const WelcomeScreen = ({ onLoadingComplete }) => {
             {/* Website Link */}
             <div className="mt-4">
               <a
-                href="https://ifaz.top"
+                href="https://ifaz.me"
                 className="flex items-center space-x-2 text-primary hover:underline transition-all group"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <Globe className="w-4 h-4 group-hover:translate-x-1 transition-transform" />
                 <span className="text-lg font-medium tracking-wide font-mono">
-                  <TypewriterEffect text="ifaz.top" />
+                  <TypewriterEffect text="ifaz.me" />
                 </span>
               </a>
             </div>
